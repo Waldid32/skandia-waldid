11b395a84198ccd2ff9d2e126b8df1286e517479
Waldid Barrios <waldid1817@gmail.com>
2024-10-17 00:40:51 -0500

    feat: subMenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { HeaderComponentSubMenu } from "./components/headerSubMenu/subMenu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, HeaderComponentSubMenu],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
