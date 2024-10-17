cd575069ef024ed1a465d3a7ea6b09e436f446dd
Waldid Barrios <waldid1817@gmail.com>
2024-10-17 00:40:02 -0500

    feat: subMenu

---
+++ src/app/components/headerSubMenu/subMenu.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { NgClass, NgFor, NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-subMenu',
+  standalone: true,
+  imports: [NgIf, NgClass, NgFor],
+  template: `
+    <div class="subMenuContainer" [ngClass]="{ collapsed: !isExpanded }">
+      <!-- Ícono de home que controla el estado del menú -->
+      <div class="menu-header" (click)="toggleMenu()">
+        <div>
+          <img src="assets/subMenu/home.svg" alt="Home" class="icon" />
+          <span *ngIf="isExpanded" class="textHome">Inicio</span>
+        </div>
+      </div>
+
+      <!-- Opciones del menú -->
+      <div class="menu-item" *ngFor="let item of menuItems">
+        <img
+          [src]="item.icon"
+          alt="{{ item.label }}"
+          class="icon"
+          *ngIf="isExpanded"
+        />
+        <span *ngIf="isExpanded">{{ item.label }}</span>
+      </div>
+    </div>
+  `,
+  styleUrls: ['./subMenu.component.scss'],
+})
+export class HeaderComponentSubMenu {
+  isExpanded = false;
+
+  menuItems = [
+    { label: 'Contratos', icon: 'assets/subMenu/hoja1.svg' },
+    { label: 'Acciones', icon: 'assets/subMenu/tool.svg' },
+    { label: 'Objetivos', icon: 'assets/subMenu/star.svg' },
+    { label: 'Herramientas', icon: 'assets/subMenu/money.svg' },
+    {
+      label: 'Servicio al cliente',
+      icon: 'assets/subMenu/talk2.svg',
+    },
+  ];
+
+  toggleMenu() {
+    this.isExpanded = !this.isExpanded;
+  }
+}
