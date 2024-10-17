6ed25a9553a1316b224ba1c9211cea29495b4d9f
Waldid Barrios <waldid1817@gmail.com>
2024-10-17 14:25:00 -0500

    fix: bug en position del footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -4,56 +4,62 @@ import { Component } from '@angular/core';
   selector: 'app-footer',
   standalone: true,
   template: `
-    <footer class="containerFooter">
-      <div class="containerLogo">
+    <footer class="container-footer">
+      <div class="container-logo">
         <img
           src="assets/header/skandia-logo.svg"
-          alt="Skandia logo"
+          alt="Logo de Skandia"
           class="logo"
         />
         <span>© 2019 Skandia</span>
-        <div class="containerDatos">
+        <div class="container-datos">
           <span>
-            PBX: <span class="textColor">658 4000</span> /
-            <span class="textColor">484 1300</span>
+            PBX: <span class="text-highlight">658 4000</span> /
+            <span class="text-highlight">484 1300</span>
           </span>
-          <span
-            >Línea nacional <span class="textColor">01 8000 517 526</span></span
-          >
+          <span>
+            Línea nacional <span class="text-highlight">01 8000 517 526</span>
+          </span>
           <span>Línea Corporativa 658 4123</span>
           <span>Av. 19 # 109A - 30 Bogotá D.C., Colombia</span>
-          <span class="textOficina"
-            ><strong>Oficinas a nivel nacional</strong></span
-          >
+          <span class="text-oficina">
+            <strong>Oficinas a nivel nacional</strong>
+          </span>
         </div>
       </div>
 
-      <div class="menuFooter">
-        <a href="#">Términos y Condiciones Canales de Servicio</a>
+      <nav class="menu-footer" aria-label="Menú del footer">
+        <a href="#">Términos y Condiciones</a>
         <a href="#">Defensoría del Consumidor Financiero</a>
         <a href="#">Protección de Datos</a>
-        <a href="#">Definiciones Generales - Auto declaración FATCA y CRS</a>
+        <a href="#">Declaración FATCA y CRS</a>
         <a href="#">Recomendaciones de Seguridad</a>
         <a href="#">Ley de Transparencia</a>
         <a href="#">Mapa del sitio</a>
-      </div>
+      </nav>
 
-      <div class="containerRedesSociales">
-        <img
-          src="assets/footer/facebookl.svg"
-          alt="Facebook logo"
-          class="iconRedesSocial"
-        />
-        <img
-          src="assets/footer/youtube.svg"
-          alt="Youtube logo"
-          class="iconRedesSocial"
-        />
-        <img
-          src="assets/footer/twitter.svg"
-          alt="Tiwtter logo"
-          class="iconRedesSocial"
-        />
+      <div class="container-redes-sociales">
+        <a href="#" aria-label="Facebook">
+          <img
+            src="assets/footer/facebook.svg"
+            alt="Facebook"
+            class="icon-redes-social"
+          />
+        </a>
+        <a href="#" aria-label="YouTube">
+          <img
+            src="assets/footer/youtube.svg"
+            alt="YouTube"
+            class="icon-redes-social"
+          />
+        </a>
+        <a href="#" aria-label="Twitter">
+          <img
+            src="assets/footer/twitter.svg"
+            alt="Twitter"
+            class="icon-redes-social"
+          />
+        </a>
       </div>
     </footer>
   `,
