83be641e43370c06201593bb28a3c48ea9b6429e
Waldid Barrios <waldid1817@gmail.com>
2024-10-17 10:41:05 -0500

    style y refactory: responsive header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -12,44 +12,41 @@ import { MenuService } from '../../services/menu.service';
         <img src="assets/header/menu-icon.svg" alt="Menu icon" />
       </div>
 
-      <div class="headerContainerSegundario">
-        <div class="containerLogo">
-          <img src="assets/header/skandia-logo.svg" alt="Skandia logo" />
-          <span class="separadorLogo"><strong>|</strong></span>
-          <span>Plan Financiero Digital - FPX</span>
+      <div class="containerLogo">
+        <img src="assets/header/skandia-logo.svg" alt="Skandia logo" />
+        <span class="separadorLogo"><strong>|</strong></span>
+        <span>Plan Financiero Digital - FPX</span>
+      </div>
+
+      <div class="containerMenu">
+        <div>
+          <img
+            src="assets/header/happy-icon.svg"
+            alt="Happy icon"
+            class="icon"
+          />
+          <a href="#">Tu Financial Planner</a>
         </div>
 
-        <div class="rightMenu">
-          <div>
-            <img
-              src="assets/header/happy-icon.svg"
-              alt="Happy icon"
-              class="icon"
-            />
-            <a href="#">Tu Financial Planner</a>
-          </div>
+        <div>
+          <img
+            src="assets/header/aportes-icon.svg"
+            alt="Aportes icon"
+            class="icon"
+          />
+          <a href="#">Aportes</a>
+        </div>
 
-          <div>
-            <img
-              src="assets/header/aportes-icon.svg"
-              alt="Aportes icon"
-              class="icon"
-            />
-            <a href="#">Aportes</a>
-          </div>
-
-          <div (mouseenter)="showHelp()" (mouseleave)="hideHelp()">
-            <img
-              src="assets/header/account-circle-icon.svg"
-              alt="Happy icon"
-              class="icon"
-            />
-            <a href="#" [ngClass]="{ visible: isShowHelp, hidden: !isShowHelp }"
-              >Preguntas <br />
-              Frecuentes</a
-            >
-            <!-- pendiente el enalce debe aparacer cuando se haga hover en la imagen -->
-          </div>
+        <div (mouseenter)="showHelp()" (mouseleave)="hideHelp()">
+          <img
+            src="assets/header/account-circle-icon.svg"
+            alt="Happy icon"
+            class="icon"
+          />
+          <a href="#" [ngClass]="{ visible: isShowHelp, hidden: !isShowHelp }"
+            >Preguntas <br />
+            Frecuentes</a
+          >
         </div>
       </div>
     </header>
